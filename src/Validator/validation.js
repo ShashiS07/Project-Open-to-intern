8c2697fbd4bbe865eab252ac74cb252b9b02da07
ShashiS07 <shashiss0708@gmail.com>
2022-11-22 13:49:03 +0530

    all tes cases handled

--- src/Validator/validation.js
+++ src/Validator/validation.js
@@ -2,7 +2,7 @@ const internModel=require('../Model/internModel')
 const collegeModel=require('../Model/collegeModel')
 
 // =========================regex validation===========================================
-
+const isvalidName=(/^([a-zA-Z_]+\s)*[a-zA-Z_]{2,50}$/)
 const isvalidChar=(/^([a-zA-Z,-:_]+\s)*[a-zA-Z_]{2,50}$/)
 const isvalidlink=(/^https?:\/\/.*\.[s3].*\.(png|gif|webp|jpeg|jpg)\??.*$/gim)
 const isvalidEmail=(/^\w+([\.]?\w+)@\w+([\.]?\w+)(\.\w{2,3})+$/)
@@ -20,7 +20,7 @@ const validationforcollege= async function(req,res,next){
         if(!name){ 
             return res.status(400).send({status:false, message:"Name is required"})
         }else{
-            if(!isvalidChar.test(name)) return res.status(400).send({status:false,error:"This name contains certain characters that aren't allowed"})
+            if(!isvalidName.test(name)) return res.status(400).send({status:false,error:"This name contains certain characters that aren't allowed"})
     
             let duplicateName=await collegeModel.find({name:name})
             if(duplicateName.length>0) return res.status(400).send({status:false, error:"This name is already taken"})
@@ -57,7 +57,7 @@ const validationforintern=async function(req,res,next){
         if(!name){ 
             return res.status(400).send({status:false, message:"Name is required"})
         }else{
-            if(!isvalidChar.test(name)) return res.status(400).send({status:false,error:"This name contains certain characters that aren't allowed"})
+            if(!isvalidName.test(name)) return res.status(400).send({status:false,error:"This name contains certain characters that aren't allowed"})
         }
         if(!email){
             return res.status(400).send({status:false,message:"email is required"})
@@ -85,7 +85,7 @@ const validationforintern=async function(req,res,next){
         if(!collegeName || collegeName==""){
         return res.status(400).send({status:false,message:"College Name is required"})
         }else{
-            if(!isvalidChar.test(collegeName)){
+            if(!isvalidName.test(collegeName)){
                 return res.status(400).send({status:false,message:"Please provide valid college name"})
             }
         }
